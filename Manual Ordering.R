cbfc60c8dfd1352948aedb8e7d61fc671f3e437d
AvarGoksu <57748290+AvarGoksu@users.noreply.github.com>
2021-08-31 10:05:10 +0200

    Update and rename Manual Ordering.Rmd to Manual Ordering.R

--- Manual Ordering.R
+++ Manual Ordering.R
@@ -1,8 +1,8 @@
-```{r}
 #If you have not so many samples, you can manually reorder the count matrix and coldata when necessary
 
 #Manually type the order and assign it to the variable "order"
-order <- c("Sample_MUC26944","Sample_MUC26945","Sample_MUC26951","Sample_MUC26952","Sample_MUC26953", "Sample_MUC26950", "Sample_MUC26954", "Sample_MUC26956", "Sample_MUC26957", "Sample_MUC26958", "Sample_MUC26946", "Sample_MUC26947", "Sample_MUC26948", "Sample_MUC26949", "Sample_MUC26955")
+order <- c("Sample_MUC26944","Sample_MUC26945","Sample_MUC26951","Sample_MUC26952","Sample_MUC26953", "Sample_MUC26950", "Sample_MUC26954", 
+"Sample_MUC26956", "Sample_MUC26957", "Sample_MUC26958", "Sample_MUC26946", "Sample_MUC26947", "Sample_MUC26948", "Sample_MUC26949", "Sample_MUC26955")
 
 #Order the count matrix
 cnt.filt <- cnt.filt[,order]
@@ -12,6 +12,5 @@ coldata <- coldata[order,]
 
 #Check if they are in the same order to make sure
 all(rownames(coldata) == colnames(cnt.filt))
-```
 
 
