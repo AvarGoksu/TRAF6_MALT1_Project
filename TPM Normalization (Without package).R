b743e279e7bb99fdc21c6d84e4521990bf1ffdec
AvarGoksu <57748290+AvarGoksu@users.noreply.github.com>
2021-08-31 09:46:41 +0200

    Add files via upload

---
+++ TPM Normalization (Without package).R
@@ -0,0 +1,12 @@
+#Make the count matrix a data frame
+countdata <- data.frame( GeneID=rownames(cnt), cnt)
+countdata <- geneLength %>%
+  inner_join(countdata) %>%
+  mutate(Length=ifelse(Length-151+1>0,Length-151+1,1)) #If Length-ReadLength+1 is
+#greater than 0, effective length is Length-ReadLength+1. Otherwise it is just 1.
+
+tpm<-countdata %>%
+  pivot_longer( cols = starts_with("Sample"), names_to = "sample", 
+                values_to = "cnt") %>% 
+  group_by(sample) %>%
+  mutate(rpk = cnt/ (Length/1000) , libSize = sum(rpk), tpm = rpk / libSize *1e6 )
